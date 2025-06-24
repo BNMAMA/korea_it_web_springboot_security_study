3c3b5d0378fc44da4c64930f2a60b59903075719
unknown <ksuyeon56@naver.com>
2025-06-24 23:23:55 +0900

    Feat:secret

--- src/main/java/com/koreait/SpringSecurityStudy/config/SecurityConfig.java
+++ src/main/java/com/koreait/SpringSecurityStudy/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.koreait.SpringSecurityStudy.config;
 
-import jakarta.websocket.Session;
+import com.koreait.SpringSecurityStudy.security.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,27 +17,36 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    //비밀번호를 안전하게 암호화(해싱)하고, 검증하는 역할
+    //단방향 해시, 복호화 불가능
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     //corsConfigurationSource() 설정은 spring security에서
     //CORS(Cross-Origin Resource Sharing)를 처리하기 위한 설정
     //CORS
     //브라우저가 보안상 다른 도메인의 리소스 요청을 제한하는 정책
     //기본적으로 브라우저는 같은 출처(Same-Origin)만 허용한다.
     @Bean
-    public CorsConfigurationSource cousConfigurationSource () {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 요청을 보내는 쪽의 도메인 (사이트 주소)을 허용하겠다
+        // 요청을 보내는 쪽의 도메인(사이트 주소)을 허용하겠다
         corsConfiguration.addAllowedOriginPattern(CorsConfiguration.ALL);
-        //요청을 보내는 쪽에서 Request, Response Header정보에 대한 제약을 허용
+        // 요청을 보내는 쪽에서 Request, Response Header 정보에 대한 제약을 허용
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-        //요청을 보내는 쪽의 메소드 (GET, POST, PUT, DELETE, OPTION등) 허용
+        // 요청을 보내는 쪽의 메소드 (GET, POST, PUT, DELETE, OPTION 등) 허용
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
 
-        //요청 URL(/user/get)에 대한 CORS설정 적용을 위한 객체 생성
+        // 요청 URL (/user/get)에 대한 CORS 설정 적용을 위해 객체 생성
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        // 모든 URL(/**)에 대해 위에서 설정하 CORS 정책을 적용
+        // 모든 URL(/**)에 대해 위에서 설정한 CORS 정책을 적용
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
-
     }
 
     @Bean
@@ -54,9 +66,11 @@ public class SecurityConfig {
         http.logout(logout -> logout.disable());
         http.sessionManagement(Session -> Session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         // 특정 요청 URL에 대한 권한 설정
         http.authorizeHttpRequests(auth -> {
-        //            auth.requestMatchers("/post").permitAll();
+            auth.requestMatchers("/auth/test", "/auth/signup").permitAll();
             auth.anyRequest().authenticated();
         });
 
